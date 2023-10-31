38646684fdb48ef21a38d8ecc294b5e88cffd29e
SHerlihy <steven_herlihy@yahoo.com>
2023-10-31 11:01:43 +0000

    strings to graphs

---
+++ createGraphs.js
@@ -0,0 +1,38 @@
+import mermaid from './node_modules/mermaid/dist/mermaid.esm.mjs';
+import {defStrings} from './defStrings.js';
+
+const content = new String(defStrings)
+
+mermaid.initialize({ 
+    startOnLoad: false,
+});
+
+const createGraphDiv = async (graphString) => {
+        const preTag = document.createElement('pre')
+    
+        const { svg } = await mermaid.render('graphDiv', graphString)
+        preTag.innerHTML = svg
+
+        document.body.append(preTag)
+}
+
+const patternGraphStart = /graph TD/g
+const graphStarts = [...content.matchAll(patternGraphStart)]
+
+const graphStrs = new Array()
+let prevStartIdx = graphStarts[0].index
+for (let i=1; i<graphStarts.length; i++) {
+    const curStartIdx = graphStarts[i].index
+    const graphStr = content.slice(prevStartIdx, curStartIdx)
+    graphStrs.push(graphStr)
+
+    prevStartIdx = curStartIdx
+}
+//may need to trim ending whitespace
+
+const graphStr = content.slice(prevStartIdx, content.length)
+graphStrs.push(graphStr)
+
+for (let i=0; i<graphStrs.length; i++) {
+    createGraphDiv(new String(graphStrs[i]))
+}
---
+++ defStrings.js
@@ -0,0 +1,1 @@
+export let defStrings = "graph TD\nA[Poo] --> B[Load Balancer]\ngraph TD\nA[Client] --> B[Load Balancer]";
